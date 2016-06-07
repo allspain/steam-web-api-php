0f908bc837ce912cab329dee1229fa599e2c443e
Richard Klein <rickaklein@gmail.com>
2016-06-06 19:09:09 -0700

    has been traded stuff

--- SteamApi/Containers/Item.php
+++ SteamApi/Containers/Item.php
@@ -27,4 +27,8 @@ class Item {
 			$this->attributes  = isset($item->attributes) ? $item->attributes : null;
 			$this->equipped    = isset($item->equipped) ? $item->equipped : null;
 	}
+
+	public function hasBeenTraded() {
+		return $this->id !== $this->originalId;
+	}
 }
